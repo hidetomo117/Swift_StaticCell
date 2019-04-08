197418158c2905285cb3d110e18fb61c1a0ae3a6
hidetomo117 <hidetomo117@gmail.com>
2019-04-08 22:05:03 +0900

    static cellとやらを使ってみた

---
+++ Swift_StaticCell/ViewControllers/Main/MainTableViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainTableViewController.swift
+//  Swift_StaticCell
+//
+//  Created by hidetomo on 2019/04/08.
+//  Copyright © 2019 Hidetomo Masuda. All rights reserved.
+//
+
+import UIKit
+
+final class MainTableViewController: UITableViewController {
+
+    // MARK: - View life cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
