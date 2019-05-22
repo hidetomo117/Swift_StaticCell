24f44231976fa9ed742631878051334109c72970
hidetomo117 <hidetomo117@gmail.com>
2019-05-23 00:40:05 +0900

    add constraints that is tableView scroll to not walking

--- Swift_StaticCell/ViewControllers/Detail/DetailTableViewController.swift
+++ Swift_StaticCell/ViewControllers/Detail/DetailTableViewController.swift
@@ -9,10 +9,20 @@
 import UIKit
 
 final class DetailTableViewController: UITableViewController {
-
+    
     // MARK: - View life cycle
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setup()
+    }
+}
+
+private extension DetailTableViewController {
+    
+    private func setup() {
+        // Static Celljは基本的にIBで設定できるが、スクロールイベントに関してはコード設定する必要あり
+        tableView.isScrollEnabled = false
     }
 }
